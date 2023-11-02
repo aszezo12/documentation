99ce0164c45ce3be9c9ea412045bf0810b3cfefa
As <146797586+aszezo12@users.noreply.github.com>
2023-11-02 09:36:34 +0400

    Delete src directory

--- src/constants.js
+++
@@ -1,9 +0,0 @@
-export const HEADER_HEIGHT = 56
-
-export const HEADER_BAR = 10
-
-export const FULL_HEADER_HEIGHT = HEADER_HEIGHT + HEADER_BAR
-
-export const SCROLL_MARGIN_TOP = FULL_HEADER_HEIGHT + 24
-
-export const CLI_PATH = '/cli'
